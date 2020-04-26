eb58567c648f383c3c7fa9f3984af09397f81bdd
cocafe <cocafehj@gmail.com>
2020-04-26 22:07:35 +0800

    app_fn: speed up fn title
    long duration may confuse user
    
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- midware/src/app_fn.c
+++ midware/src/app_fn.c
@@ -10,7 +10,7 @@
 #include "app_timer.h"
 
 #define FN_SHOW_LCD_STAY_MS             (20)
-#define FN_TITLE_LCD_STAY_MS            (1000)
+#define FN_TITLE_LCD_STAY_MS            (250)
 
 static int8_t fn_idx;
 static int8_t last_fn = -1;
