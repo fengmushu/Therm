d847f2cd329c0e9626d307b84db93960a93d6041
ken <fengmushu@gmail.com>
2020-05-10 10:52:18 +0800

    AMP limits to 100-1000

--- midware/src/tnna.c
+++ midware/src/tnna.c
@@ -353,7 +353,7 @@ boolean_t NNA_Calibration(
     {
         // 线性区间放大
         fAmp = (uVAdcH - uVAdcL) / (fTH - fTL);
-        if (fAmp >= 200 && fAmp <= 1000)
+        if (fAmp >= 100 && fAmp <= 1000)
         {
             fCaLBase = uVAdcL / fAmp - fTL;
             fCaHBase = uVAdcH / fAmp - fTH;
